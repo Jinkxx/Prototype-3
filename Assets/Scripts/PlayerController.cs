61d41aa3f89d54c84332b0b72d1c7fb156979e7c
Jinkxx <everythinginreturninfo.com>
2026-08-18 20:18:07 -0400

    playercontroller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public InputAction jumpAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
+
+        jumpAction.Enable();
+
+
+
     }
 
     // Update is called once per frame
