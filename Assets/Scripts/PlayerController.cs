c060529cd8e82548e00c8d3d5303cbaeba39e017
Jinkxx <everythinginreturninfo.com>
2026-08-20 10:46:39 -0400

    updates to playercontroller for jump control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,23 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public InputAction jumpAction;
+    public float jumpForce = 10;
+    public float gravityModifier;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 1000);
-
+        //equals physics.gravity = physics.gravity * gravityModifier;
+        Physics.gravity *= gravityModifier;
         jumpAction.Enable();
-
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (jumpAction.triggered)
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
     }
 }
