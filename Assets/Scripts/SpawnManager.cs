09e264834743355a9a961a378d8d7f6927980861
Jinkxx <everythinginreturninfo.com>
2026-09-19 11:11:57 -0400

    adding stop logic to obstacles

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,12 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);   
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player1").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -19,7 +21,10 @@ public class SpawnManager : MonoBehaviour
     }
 
     void SpawnObstacle()
-    {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    {  
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
