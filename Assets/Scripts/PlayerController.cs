507c2d64d5f3d5ed88e1f298e10f7e22a64830c8
Jinkxx <everythinginreturninfo.com>
2026-09-19 11:04:22 -0400

    added logic to stop moveleft when player collides withobstacle. Need to do same for spawnmanager and player actions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
-    public bool gameOver;
+    public bool gameOver; //made public so other scripts access the player controller script to check if the game is over or not
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
